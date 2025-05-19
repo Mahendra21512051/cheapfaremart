35a41a49dabdd91d1cf6f84b6a384ad89a3d401d
Mahendra21512051 <mahendramaury420@gmail.com>
2025-05-19 11:59:34 +0530

    Updated files with recent edits

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
     <>
       <h1 className="bg-green-700">welcom to  my Home nishan</h1> 
       <p>Its changing</p> 
+      <p>its change mahendra</p>
       <Header/>     
     </>
   )
