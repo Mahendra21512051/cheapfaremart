43d4d546ad4805640653120df4337b541efd43f0
nishan-khiva <nishankhiva@gmail.com>
2025-05-19 11:53:04 +0530

    nishan ne change kiya

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,8 @@ import Header from '../src/component/Header';
 function App() {
   return (
     <>
-      <h1 className="bg-green-700">welcom to  my Home nishan</h1>  
+      <h1 className="bg-green-700">welcom to  my Home nishan</h1> 
+      <p>Its changing</p> 
       <Header/>     
     </>
   )
